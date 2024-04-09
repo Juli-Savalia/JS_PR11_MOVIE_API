10add2470bcb8d073969ab8df0250405fcd6c95c
Juli <3382julisavalia@gmail.com>
2024-04-09 10:19:29 +0530

    api

--- api.js
+++ api.js
@@ -1,20 +1,38 @@
 $.ajax({
-    url : "https://dummyjson.com/carts",
-    type : "GET",
-    success : function(res){
-        let tbl="";
-       res.map((val)=>{
-            return (
+    type: "GET",
+    url: "https://dummyjson.com/carts",
+    success: function (res) {
+        let tbl = "";
+        let carts = res.carts;
+        console.log(carts);
+        carts.map((val) => {
+        
                 tbl += `
-                    <tr>
-                        <td>${val.id}</td>
-                        <td>${val.name}</td>
-                        <td>${val.email}</td>
-                        <td>${val.body}</td>
-                    </tr>
-                `
-            )
-       })
-       document.getElementById('data').innerHTML = tbl;
+                       <div class = "d-flex flex-wrap">
+                            <div class="card w-100 py-3" style="width: 18rem;">
+                                <div class = "d-flex">
+                        `
+                 let cartsproduct = val.products;
+
+                 cartsproduct.map((items)=>{
+                    tbl+=  `
+                                    <div class="col-20 d- row">
+                                    <img src="${items.thumbnail}" class="card-img-top" alt="..." width:50%>
+                                    <div class="card-body">
+                                        <h5 class="card-title">${items.title}</h5>
+                                        <p class="card-text">${val.total}</p>
+                                        <p class="card-text">${val.discountedTotal}</p>
+                                        <a href="#" class="btn btn-primary">Add To Cart</a>
+                                    </div>
+                                    </div>
+                                 </div>
+                            </div>
+                        </div>
+                         `
+                 })
+                        
+            
+        })
+        document.getElementById('data').innerHTML = tbl;
     }
 })
